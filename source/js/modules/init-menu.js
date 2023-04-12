dc88f85cc9eebd547d79b37c0de7a9f4cda4d6ae
Anastasia <anastasia.venediktova1329@gmail.com>
2023-04-12 20:52:40 +0300

    fix

--- source/js/modules/init-menu.js
+++ source/js/modules/init-menu.js
@@ -1,7 +1,6 @@
 const initMenu = () => {
   let nav = document.querySelector('.navigation');
   let navToggle = document.querySelector('.navigation__button');
-  let body = document.querySelector('body');
 
   nav.classList.remove('navigation--nojs');
 
@@ -9,11 +8,9 @@ const initMenu = () => {
     if (nav.classList.contains('navigation--closed')) {
       nav.classList.remove('navigation--closed');
       nav.classList.add('navigation--opened');
-      body.classList.add('page--opened');
     } else {
       nav.classList.add('navigation--closed');
       nav.classList.remove('navigation--opened');
-      body.classList.remove('page--opened');
     }
   });
 };
